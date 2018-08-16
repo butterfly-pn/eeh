aa1d7a5ce24f6927b8b7a00e77b92dbf5ad8d6a8
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2018-08-16 15:03:14 +0200

    Switch home & app path

--- eeh/views/app.py
+++ eeh/views/app.py
@@ -1,6 +1,14 @@
-from flask import render_template
+from flask import render_template, redirect, flash
+from flask_login import current_user
 from main import APP
+from eeh.models import Harcerz
 
-@APP.route('/app/', methods=["GET"])
+
+@APP.route('/app/')
 def app():
-    return render_template('app.html')
+    if current_user.is_authenticated:
+        harcerze = Harcerz.query.filter_by(druzyna=current_user.id).all()
+    else:
+        flash("Zaloguj się", "warning")
+        return redirect('/')
+    return render_template('app.html', harcerze=harcerze)
--- eeh/views/home.py
+++ eeh/views/home.py
@@ -1,13 +1,7 @@
-from flask import render_template, redirect, flash
-from flask_login import current_user
+from flask import render_template
 from main import APP
-from eeh.models import Harcerz
 
-@APP.route('/')
-def homepage():
-    if current_user.is_authenticated:
-        harcerze = Harcerz.query.filter_by(druzyna=current_user.id).all()
-    else:
-        flash("Zaloguj się", "warning")
-        return redirect('/app/')
-    return render_template('homepage.html', harcerze=harcerze)
+
+@APP.route('/', methods=["GET"])
+def home():
+    return render_template('home.html')
