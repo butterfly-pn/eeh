b0dfaed38686c7551196688d8d9579513214ff25
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2018-09-02 19:09:45 +0200

    Work plan

--- eeh/views/plan.py
+++ eeh/views/plan.py
@@ -5,25 +5,7 @@ from eeh.view_manager import login_required
 from main import APP, DB
 
 @login_required
-@APP.route('/plan/', methods=['GET'])
-def default_plan():
-    harcerze = []
-    current_plan = Plan.query.filter_by(id=current_user.current_plan).first()
-    plany = Plan.query.filter_by(druzyna_id=current_user.id).all()
-    plany_indywidualne = HarcerzPlan.query.filter_by(plan_id=current_user.current_plan).all()
-    for plan_indywidualny in plany_indywidualne:
-        harcerz = Harcerz.query.filter_by(id=plan_indywidualny.id).first()
-        harcerze.append({
-            'first_name' : harcerz.first_name, 
-            'last_name' : harcerz.last_name, 
-            'charakterystyka' : plan_indywidualny.charakterystyka, 
-            'cele' : plan_indywidualny.cele
-            })
-    return render_template('plan.html', harcerze=harcerze, wizja=current_plan.wizja, cele=current_plan.cele, zz=current_plan.zz, plany=plany)
-
-
-@login_required
-@APP.route('/plan/<identifier>', methods=['GET'])
+@APP.route('/plan/<identifier>/', methods=['GET'])
 def plan(identifier):
     harcerze = []
     current_plan = Plan.query.filter_by(id=identifier).first()
@@ -42,6 +24,20 @@ def plan(identifier):
         return render_template('plan.html', harcerze=harcerze, wizja=current_plan.wizja, cele=current_plan.cele, zz=current_plan.zz, plany=plany)
     flash("You can't do that", 'warning')
     return redirect(request.host_url)
+
+@login_required
+@APP.route('/plan/<identifier>/delete/', methods=['GET'])
+def plan_delete(identifier):
+    plan = Plan.query.filter_by(id=identifier).first()
+    if plan.druzyna_id == current_user.id or current_user.admin:
+        if plan.id == current_user.current_plan:
+            flash("To jest twój aktualny plan!", 'warning')
+            return redirect('/plan/'+str(identifier))
+        DB.session.delete(plan)
+        DB.session.commit()
+        flash("Usunięto "+plan.name, 'success')
+    return redirect('/app/')
+        
 
 @login_required
 @APP.route('/plan/new/', methods=['GET'])
