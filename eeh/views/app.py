bade0dd08138a4807d3d3f1cc2b069b0af361daa
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2018-09-09 18:22:01 +0200

    Fixed links in app view

--- eeh/views/app.py
+++ eeh/views/app.py
@@ -14,7 +14,7 @@ def app():
         query = con.execute(sql, escape_string(str(current_user['id_user'])))
         scout_team = con.fetchone()
         if not query == 0:
-            sql = "SELECT a.first_name, a.middle_name, a.last_name, a.birthdate, a.pesel, a.address, a.phone, b.scouting_troop_id, c.name as 'scouting_troop_name', c.scout_team_id FROM scout a, scout_membership b, scouting_troop c WHERE c.scout_team_id = %s AND b.scouting_troop_id = c.id_scouting_troop AND a.id_scout = b.scout_id"
+            sql = "SELECT a.id_scout, a.first_name, a.middle_name, a.last_name, a.birthdate, a.pesel, a.address, a.phone, b.scouting_troop_id, c.name as 'scouting_troop_name', c.scout_team_id FROM scout a, scout_membership b, scouting_troop c WHERE c.scout_team_id = %s AND b.scouting_troop_id = c.id_scouting_troop AND a.id_scout = b.scout_id"
             query = con.execute(sql, escape_string(str(scout_team['id_scout_team'])))
             scouts_raw = con.fetchall()
             print(scouts_raw)
