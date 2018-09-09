e8a7a195c59c858de44eba59a95be0896274bd92
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2018-09-09 18:50:03 +0200

    New database model

--- eeh/sql/statuses.sql
+++ eeh/sql/statuses.sql
@@ -1,3 +1,3 @@
-insert into status (name, description) values ("unverified", "User is unverified, can login, but can't do most of operations");
-insert into status (name, description) values ("verified", "User is verified, can do everything what a normal user can do");
-insert into status (name, description) values ("admin", "User is an admin");
+insert into status (name, description) values ("active", "Record is active");
+insert into status (name, description) values ("unactive", "Record isn't removed but has restricted access");
+insert into status (name, description) values ("removed", "Record is removed");
