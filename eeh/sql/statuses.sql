1cd76210959a1a7e60d37a1f0f6a2555efc3eebc
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2018-09-09 14:59:50 +0200

    sql folder

---
+++ eeh/sql/statuses.sql
@@ -0,0 +1,3 @@
+insert into status (name, description) values ("unverified", "User is unverified, can login, but can't do most of operations");
+insert into status (name, description) values ("verified", "User is verified, can do everything what a normal user can do");
+insert into status (name, description) values ("admin", "User is an admin");
