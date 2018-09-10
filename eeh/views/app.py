1eec4520554ec4e7dd9e3f58648fb46d5766d71e
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2018-09-10 11:21:56 +0200

    App access to all from komenda

--- eeh/views/app.py
+++ eeh/views/app.py
@@ -10,24 +10,28 @@ def app():
     scouts = []
     no_team = True
     con, conn = connection()
-    sql = "SELECT id_scout_team, name FROM scout_team WHERE scoutmaster_user_id = %s"
     if current_user.is_authenticated:
-        query = con.execute(sql, escape_string(str(current_user['id_user'])))
+        sql = "SELECT a.id_scout_team, a.name, c.scouting_troop_id, b.name FROM scout_team a, scouting_troop b, scout_membership c WHERE c.scouting_troop_id = b.id_scouting_troop AND a.id_scout_team = b.scout_team_id AND c.scout_id = %s"
+        query = con.execute(sql, current_user['scout_id'])
         scout_teams = con.fetchall()
         if not query == 0:
-            sql = "SELECT a.id_scout, a.first_name, a.middle_name, a.last_name, a.birthdate, a.pesel, a.address, a.phone, b.scouting_troop_id, c.name as 'scouting_troop_name', c.scout_team_id FROM scout a, scout_membership b, scouting_troop c WHERE c.scout_team_id = %s AND b.scouting_troop_id = c.id_scouting_troop AND a.id_scout = b.scout_id"
-            id = scout_teams[0]['id_scout_team']
-            if request.args.get('scout-team'):
-                query = con.execute("SELECT id_scout_team FROM scout_team WHERE id_scout_team = %s AND scoutmaster_user_id = %s",
-                            (escape_string(request.args.get('scout-team')), escape_string(str(current_user['id_user']))))
-                if not query == 0:
-                    id = int(request.args.get('scout-team'))
-            query = con.execute(sql, escape_string(str(id)))
-            scouts_raw = con.fetchall()
-            print(scouts_raw)
+            sql = "select a.id_scout_team, a.name, b.id_scouting_troop, c.scout_id FROM scout_team a, scouting_troop b, scout_membership c WHERE a.id_scout_team = b.scout_team_id AND c.scouting_troop_id = b.id_scouting_troop AND b.name = \"Komenda\" AND c.scout_id = %s;"
+            query = con.execute(sql, escape_string(str(current_user['id_user'])))
+            scout_teams = con.fetchall() if not query==0 else scout_teams
             if not query == 0:
-                scouts = scouts_raw
-            no_team = False
+                sql = "SELECT a.id_scout, a.first_name, a.middle_name, a.last_name, a.birthdate, a.pesel, a.address, a.phone, b.scouting_troop_id, c.name as 'scouting_troop_name', c.scout_team_id FROM scout a, scout_membership b, scouting_troop c WHERE c.scout_team_id = %s AND b.scouting_troop_id = c.id_scouting_troop AND a.id_scout = b.scout_id"
+                id = scout_teams[0]['id_scout_team']
+                if request.args.get('scout-team'):
+                    query = con.execute("SELECT id_scout_team FROM scout_team WHERE id_scout_team = %s AND scoutmaster_user_id = %s",
+                                (escape_string(request.args.get('scout-team')), escape_string(str(current_user['id_user']))))
+                    if not query == 0:
+                        id = int(request.args.get('scout-team'))
+                query = con.execute(sql, escape_string(str(id)))
+                scouts_raw = con.fetchall()
+                print(scouts_raw)
+                if not query == 0:
+                    scouts = scouts_raw
+                no_team = False
         con.close()
         conn.close()
     else:
@@ -35,4 +39,4 @@ def app():
         conn.close()
         flash("Zaloguj się", "warning")
         return redirect('/')
-    return render_template('app.html', harcerze=scouts, no_team=no_team, scout_teams=scout_teams, current_scout_team_id=id)
+    return render_template('app.html', harcerze=scouts, no_team=no_team, scout_teams=scout_teams, current_scout_team_id=None if no_team else id)
