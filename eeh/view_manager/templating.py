96a026d021d787c9238352900977481e86e3fb91
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2018-09-14 01:03:47 +0200

    Fix AnonymusUserMixin error

--- eeh/view_manager/templating.py
+++ eeh/view_manager/templating.py
@@ -14,13 +14,14 @@ def render_template(template_name_or_list, **context):
     :param context: the variables that should be available in the
                     context of the template.
     """
-    con, conn = connection()
-    sql = "select a.id_scout_team, a.name, b.id_scouting_troop, c.scout_id FROM scout_team a, scouting_troop b, scout_membership c WHERE a.id_scout_team = b.scout_team_id AND c.scouting_troop_id = b.id_scouting_troop AND b.name = \"Komenda\" AND c.scout_id = %s;"
-    con.execute(sql, escape_string(str(current_user['id_user'])))
-    scout_teams = con.fetchall()
-    con.close()
-    conn.close()
-    context['scout_teams'] = scout_teams
+    if current_user.is_authenticated:
+        con, conn = connection()
+        sql = "select a.id_scout_team, a.name, b.id_scouting_troop, c.scout_id FROM scout_team a, scouting_troop b, scout_membership c WHERE a.id_scout_team = b.scout_team_id AND c.scouting_troop_id = b.id_scouting_troop AND b.name = \"Komenda\" AND c.scout_id = %s;"
+        con.execute(sql, escape_string(str(current_user['id_user'])))
+        scout_teams = con.fetchall()
+        con.close()
+        conn.close()
+        context['scout_teams'] = scout_teams
     ctx = _app_ctx_stack.top
     ctx.app.update_template_context(context)
     return _render(ctx.app.jinja_env.get_or_select_template(template_name_or_list),
